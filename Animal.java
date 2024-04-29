a23dfdeda9d647e15152f7983ddbe375b1e07f83
Negar-721 <yadegarinegar111@gmail.com>
2024-04-29 14:12:18 +0330

    second pull request done

--- Animal.java
+++ Animal.java
@@ -1,3 +1,42 @@
 public abstract class Animal {
 
+    String animalName;
+    int old;
+    String abilities;
+
+    public Animal(String animalName, int old, String abilities) {
+        this.animalName = animalName;
+        this.old = old;
+        this.abilities = abilities;
+    }
+
+    public void show() {
+        System.out.println(animalName + " " + old + " " + abilities);
+
+    }
+
+    public String getAnimalName() {
+        return this.animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public int getOld() {
+        return this.old;
+    }
+
+    public void setOld(int old) {
+        this.old = old;
+    }
+
+    public String getAbilities() {
+        return this.abilities;
+    }
+
+    public void setAbilities(String abilities) {
+        this.abilities = abilities;
+    }
+
 }
--- Girafe.java
+++ Girafe.java
@@ -1,2 +1,12 @@
-public class Girafe extends Mammal implements Prey{
+public class Girafe extends Mammal implements Prey {
+
+    public Girafe(String animalName, int old, String abilities, int runningVelocity) {
+        super(animalName, old, abilities, runningVelocity);
+    }
+    
+    public String getName() {
+        return animalName;
+
+    }
+
 }
